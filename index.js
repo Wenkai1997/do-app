1b92fe6fbdda6da3b39be8b3ae55e8863deea901
wenkai <75389917+Wenkai1997@users.noreply.github.com>
2021-05-23 13:24:32 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const { Telegraf } = require('telegraf');
+require('dotenv').config(); // no need to assign
+
+const bot = new Telegraf(`${process.env.TOKEN}`);
+
+bot.on('text', (ctx) =>
+  bot.telegram.sendMessage(ctx.chat.id, ctx.message.text),
+);
+
+bot
+  .launch()
+  .then(console.log('up n running!'))
+  .catch((err) => console.log(err));
